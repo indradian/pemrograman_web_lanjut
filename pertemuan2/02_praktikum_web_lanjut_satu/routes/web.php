aed54c0acb3ed974530b6a867b55b3be692c657d
= <=>
2023-02-23 07:40:59 +0700

    change routes/web.php

--- pertemuan2/02_praktikum_web_lanjut_satu/routes/web.php
+++ pertemuan2/02_praktikum_web_lanjut_satu/routes/web.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "Indrayana Dian Prasetya <br>";
+    echo "2141720132";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID:".$id;
 });
