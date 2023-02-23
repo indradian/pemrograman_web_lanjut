d5c9441200f65febb3d13784d0fb58eb1b410cc5
= <=>
2023-02-23 15:15:15 +0700

    add Controllers

---
+++ pertemuan2/02_praktikum_web_lanjut_dua/app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about() {
+        return 'Indrayana Dian Prasetya <br> 2141720232';
+    }
+}
