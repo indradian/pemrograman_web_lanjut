e7bd4ae489dfc63377d04d5b34d81144723eff6b
indradprasetya <indrayanadian7@gmail.com>
2023-06-18 17:05:22 +0700

    edit HomeController

--- pertemuan2/02_praktikum_web_lanjut_dua/app/Http/Controllers/HomeController.php
+++ pertemuan2/02_praktikum_web_lanjut_dua/app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-        return 'Selamat Datang';
+        $user = auth()->user();
+        return view('home', ['user' => $user]);
     }
 }
