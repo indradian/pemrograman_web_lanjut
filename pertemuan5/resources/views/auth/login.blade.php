ec806d5f85faf70b7b74b97779110975d3943afa
indradprasetya <indrayanadian7@gmail.com>
2023-06-18 16:47:57 +0700

    edit login

--- pertemuan5/resources/views/auth/login.blade.php
+++ pertemuan5/resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 
                             <div class="row mb-3">
                                 <label for="username"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('username') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="username" type="username"
