0030834c5f492c2aac0334e9d05e8cfe2dcf8b66
indradprasetya <indrayanadian7@gmail.com>
2023-06-18 18:36:38 +0700

    edit Mahasiswa.php

--- pertemuan7/app/Models/Mahasiswa.php
+++ pertemuan7/app/Models/Mahasiswa.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Mahasiswa extends Model
+class Mahasiswa extends Model //Definisi Model
 {
-    use HasFactory;
+    protected $table = 'mahasiswa'; //Eloquent akan membuat model mahasiswa menyimpan record di tabel mahasiswas
+    public $timestamps = false;
+    protected $primaryKey = 'Nim'; // Memanggil isi DB Dengan primarykey
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'Nim',
+        'Nama',
+        'Kelas',
+        'Jurusan',
+        'No_Handphone',
+    ];
+
 }
