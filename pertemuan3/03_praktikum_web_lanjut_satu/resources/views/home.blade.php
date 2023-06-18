65efd67100121e85e64e9acdd064806bd7d4b12c
indradprasetya <indrayanadian7@gmail.com>
2023-06-18 17:12:18 +0700

    edit home blade

--- pertemuan3/03_praktikum_web_lanjut_satu/resources/views/home.blade.php
+++ pertemuan3/03_praktikum_web_lanjut_satu/resources/views/home.blade.php
@@ -7,6 +7,19 @@
   <title>Home</title>
 </head>
 <body>
-  <h1>Tampilan Home</h1>  
+  <div class="card-body">
+    @if (session('status'))
+      <div class="alert alert-success" role="alert">
+        {{ session('status') }}
+      </div>
+    @endif
+    {{ __('You are logged in!') }}
+    <table class="table table-responsive">
+        <tr><th>Username</th><th>:</th><td>{{ $user->Username }}</td></tr>
+        <tr><th>Name</th><th>:</th><td>{{ $user->Name }}</td></tr>
+        <tr><th>Email</th><th>:</th><td>{{ $user->Email }}</td></tr>
+        <tr><th>Created At</th><th>:</th><td>{{ $user->created_at }}</td></tr>
+    </table>
+  </div>
 </body>
 </html>
